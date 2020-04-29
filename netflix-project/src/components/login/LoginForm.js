fb33c9db6515a9cd148f0a1c46a39940def8b239
Pow <my.paulshin91@gmail.com>
2020-04-29 20:31:02 +1200

    logic added to sign in form

--- netflix-project/src/components/login/LoginForm.js
+++ netflix-project/src/components/login/LoginForm.js
@@ -3,7 +3,78 @@ import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import FBlogo from '../../images/fb-logo.png';
 
+
+const regexp = RegExp(/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/);
+
+const initState = {
+    checked: true,
+    email: '',
+    password: '',
+    emailError: '',
+    passwordError: ''
+}
+
 class LoginForm extends Component {
+
+    state = initState;
+
+    handleEmailChange = e => {
+        this.setState({
+            email: e.target.value
+        });
+    }
+
+    handlePasswordChange = e => {
+        this.setState({
+            password: e.target.value
+        });
+    }
+
+    // Validate
+    validate = () => {
+        let inputError = false;
+        const errors = {
+            emailError: '',
+            passwordError: ''
+        }
+
+        if(!this.state.email) {
+            inputError = true;
+            errors.emailError = 'Please enter a valid email'
+        } else if(!this.state.email.match(regexp)) {
+            inputError = true;
+            errors.emailError = (
+                <span style={{color:'red'}}>Your email address must be valid</span>
+            )
+        }
+
+        if(this.state.password.length < 4) {
+            inputError = true;
+            errors.passwordError = "Your password must contain between 4 and 16 characters"
+        }
+
+        this.setState({
+            ...errors
+        })
+        return inputError;
+    }
+
+    onSubmit = e => {
+        e.preventDefault();
+
+        const err = this.validate();
+        if(!err) {
+            this.setState(initState);
+        }
+    }
+
+    // Checkbox
+    handlerCheckbox = e => {
+        this.setState({
+            checked: e.target.checked
+        })
+    }
+
     render() {
         return (
             <FormContainer>
@@ -11,19 +82,21 @@ class LoginForm extends Component {
                     <form>
                         <h1>Sign In</h1>
                         <div className="input-container">
-                            <input className="input-empty" type="email" required />
+                            <input className={this.state.emailError ? 'input-error input-empty' : 'input-empty'} type="email" onChange={this.handleEmailChange} value={this.state.email} required />
                             <label>Email or Phone Number</label>
+                            <span style={{color:'#db7302'}}>{this.state.emailError}</span>
                         </div>
                         <div className="input-container">
-                            <input className="input-empty" type="password" required />
+                            <input className={this.state.passwordError ? 'input-error input-empty' : 'input-empty'} type="password" onChange={this.handlePasswordChange} value={this.state.password} required />
                             <label>Password</label>
+                            <span style={{color:'#db7302'}}>{this.state.passwordError}</span>
                         </div>
                         <div className="input-container">
-                            <Button type="submit">Sign In</Button>
+                            <Button type="0" onClick={e=> this.onSubmit(e)}>Sign In</Button>
                         </div>
                         <label className="checkbox-container">
                             Remember me
-                            <input type="checkbox" checked />
+                            <input type="checkbox" defaultChecked={this.state.checked} onClick={this.handlerCheckbox} />
                             <span className="checkmark"></span>
                         </label>
                         <Link to="/" className="need-help">
@@ -101,6 +174,10 @@ input:focus ~ label {
 
 input:focus {
     outline: none;
+}
+
+.input-error {
+    border-bottom: 1px solid #db7302;
 }
 
 // checkbox
